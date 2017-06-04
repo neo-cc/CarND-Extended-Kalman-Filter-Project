505c1e3885f1f4eefb3c4f96649bec4c57e5f6f5
neo <neo@neos-iMac.local>
2017-06-03 18:49:33 -0700

    modified cmake setting to enable openssl

--- install-mac.sh
+++ install-mac.sh
@@ -4,11 +4,12 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 cd build
-cmake ..
+#cmake ..
+cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2k -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2k/lib ..
 make 
 sudo make install
 cd ..
 cd ..
-sudo rm -r uWebSockets
+#sudo rm -r uWebSockets
